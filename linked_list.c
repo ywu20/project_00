1045ac480934ed1c8f2281d411155c994205285c
Yuqing Wu <ywu20@stuy.edu>
2021-10-19 11:47:22 -0400

    print_list

--- linked_list.c
+++ linked_list.c
@@ -8,11 +8,16 @@ char artist[100];
 struct song_node *next;
 };
 
+
+
 //insert nodes at the front
+struct song_node * insert_front(struct song_node * start, char artist[100], char name[100]);
 
 //insert nodes in order, alphabetically by Artist then by Song
-//compare song nodes
+struct song_node * insert_order(struct song_node * start, char artist[100],char name[100]);
 
+//compare song nodes, returns < 0 if n1 goes before n2, if 0 the same, if > 0 n2 goes before n1
+int node_cmp (struct song_node * n1, struct song_node * n2);
 
 //print the entire list
 void print_list (struct song_node * node){
@@ -21,8 +26,20 @@ void print_list (struct song_node * node){
   node = node ->next;
 }
 }
+
+
 //find and return a pointer to a node based on artist and song name
+struct song_node * find_song(struct song_node * start, char artist[100], char name[100]);
+
 // find and return a pointer to the first song of an artist based on artist name
+struct song_node * find_artist(struct song_node * start, char artist[100]);
+
 //Return a pointer to random element in the list.
+struct song_node * rand_song(struct song_node * start);
+
 //remove a single specified node from the list, specified by both artist and song name.
+
+struct song_node * remove_node(struct song_node * start, char artist[100], char name[100]);
+
 //free the entire list
+struct song_node * free_list(struct song_node * start);
