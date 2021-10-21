70b7ef0fe6f5c4661615f3b8f90805cba884cd26
Yuqing Wu <ywu20@stuy.edu>
2021-10-21 11:47:34 -0400

    fixing order

--- linked_list.c
+++ linked_list.c
@@ -36,20 +36,36 @@ struct song_node * insert_order(struct song_node * start, char artist[100],char 
   strcpy(p->artist,artist);
   strcpy(p->name, name);
  
+ if(start == NULL){
+  return p;
+ }
  //making the strings that will be compared
-  p_str = p->artist;
-  start_str = start->artist;
-  
+ printf("====================\n");
+  char p_str[200];  
+  strncpy(p_str, p->artist, sizeof(p->artist));
+  //strcpy(p_str, p->artist);
+  printf("p_str copying artist: %s\n", p_str);
+  char start_str[200]; 
+  strncpy(start_str, start->artist, sizeof(start->artist));
+ //strcpy(start_str, start->artist);
+  printf("start_str copying artist: %s\n", start_str);
   strcat(p_str, p->name);
+  printf("p_str cating name: %s\n", p_str);
   strcat(start_str, start-> name);
+  printf("start_str cating name: %s\n", start_str);
 
-  //find correct position to insert the new struct
-  while(strcmp(p_str,start_str)<=0 && ){
-   start = start ->next;
-   start_str = start->artist;
-   strcat(start_str, start-> name);
+  //find correct position to insert the new struct, keep track of prev
+  printf ("comparison: %d\n", strcmp(p_str,start_str));
+  while(strcmp(p_str,start_str)>0){
+   start = start ->next; //next is null
+   //strncpy(start_str, start->artist, sizeof(start->artist));
+   //strcat(start_str, start-> name);
+   //printf("Start: %s, %s\n", start -> artist, start ->name);
   }
-  p->next = start;
+  
+  //p->next = start;
+  //prev -> next = p;
+  printf("====================\n");
   return p;
 }
 
